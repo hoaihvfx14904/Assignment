b6268b83c5356529cf395c456cfd5da64d86f012
hoaihvfx14904 <96828188+hoaihvfx14904@users.noreply.github.com>
2022-02-10 19:24:57 +0700

    header footer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
+
 import React, { Component} from 'react';
-import {STAFFS} from './shared/staffs.jsx';
-import Menu from './components/MenuComponents.js'
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Main from './components/MainComponent.js'
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      staffs: STAFFS,
-
-    }
-  }
   render(){
   return (
+    <BrowserRouter>
     <div className='App'>
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/'>Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu staffs={this.state.staffs}/>
-      
-     
+      <Main />
     </div>
+    </BrowserRouter>
   );
   }
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse} from "reactstrap";
+import {NavLink} from "react-router-dom";
+class Header extends Component{
+    render(){
+        return (
+            <>
+                <Navbar dark expand="md">
+                    <div className="container">
+                        <Nav navbar>
+                            <NavbarBrand className="mr-auto" href="/" >
+                                <img src="assets/images/logo.png" height="30" width="41" alt="Resibon" />
+                            </NavbarBrand>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/staff" >
+                                    <span className="fa fa-address-book-o fa-lg"></span> Nhân Viên
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/department" >
+                                    <span className="fa fa-university fa-lg"></span> Phòng Ban
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/salary" >
+                                    <span className="fa fa-cc fa-lg"></span> Bảng Lương
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </div>
+                </Navbar>
+            </>
+        )
+    }
+}
+export default Header;
